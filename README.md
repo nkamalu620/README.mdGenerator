80291e4471fe306e84eb7832dea62209e1e96bd8
nkamalu620 <nkamalu96@gmail.com>
2024-11-29 18:58:18 -0500

    My new created code

---
+++ README.md
@@ -0,0 +1,30 @@
+# Choose you next home
+
+## Description
+You are asked a seires of questions and when youare finished the app will give suggestions on where to line
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+must me installed on your browser
+
+## Usage
+must put in you general information
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+December 5
+
+## Tests
+answer the questions
+
+## Questions
+For any questions, please reach out to nkamalu620 or email me at nkamalu96@gmail.com.
---
+++ index.js
@@ -0,0 +1,104 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer').default;
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information?'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What license does your project use?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the contribution guidelines?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data.trim());
+    console.log('README.md has been generated!');
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+    .prompt(questions).then(answers => {
+        const readmeContent = `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For any questions, please reach out to ${answers.github} or email me at ${answers.email}.`;
+
+        try {
+            writeToFile('README.md', readmeContent);
+        } catch (err) {
+            console.error('Error writing to file:', err);
+        }
+    });
+}
+
+// Function call to initialize app
+init();
